dbaed5e4dcc3b5947354ba77674deddb391f3703
wesley@ufpi.edu.br <wesley@ufpi.edu.br>
2024-01-09 23:50:10 -0300

    Alteração para inclusão de envio de email pela página.

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,43 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = getenv('EMAIL');
+require 'vendor/autoload.php';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+    // Configurações do servidor SMTP
+    $mail = new PHPMailer(true);
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  
-  $contact->smtp = array(
-    'host' => getenv('SMTP'),
-    'username' => getenv('EMAIL'),
-    'password' => getenv('EMAIL_PASSWORD'), // Utiliza a variável de ambiente
-    'port' => '587'
-);
-  
+    try {
+        // Configurações do servidor SMTP
+        $mail->isSMTP();
+        $mail->Host       = dotenv('SMPT');
+        $mail->SMTPAuth   = true;
+        $mail->Username   = dotenv('EMAIL');
+        $mail->Password   = dotenv('PASS');
+        $mail->SMTPSecure = 'tls'; // tls ou ssl
+        $mail->Port       = 587;    // A porta pode variar de acordo com a configuração do seu servidor
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+        // Remetente e destinatário
+        $mail->setFrom($email, $name);
+        $mail->addAddress(dotenv('EMAIL')); // Substitua pelo seu endereço de e-mail
 
-  echo $contact->send();
+        // Conteúdo do e-mail
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body    = "Nome: $name<br>E-mail: $email<br>Assunto: $subject<br><br>Mensagem:<br>$message";
+
+        $mail->send();
+        echo "success";
+    } catch (Exception $e) {
+        echo "error";
+    }
+} else {
+    echo "error";
+}
 ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5e98267fcc627f9a1855cab073f5dcf74bf92f41',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5e98267fcc627f9a1855cab073f5dcf74bf92f41',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.1',
+            'version' => '6.9.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => '039de174cd9c17a8389754d3b877a2ed22743e18',
+            'dev_requirement' => false,
+        ),
+    ),
+);
